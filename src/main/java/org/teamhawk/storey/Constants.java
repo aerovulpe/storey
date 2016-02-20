f539bae85aeed6a6e9467c4f9f53ebbbea2df8d6
Aaron Nwabuoku <aerisvulpe@gmail.com>
2016-02-20 15:06:37 -0500

    init

---
+++ src/main/java/org/teamhawk/storey/Constants.java
@@ -0,0 +1,13 @@
+package org.teamhawk.storey;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
+ */
+public class Constants {
+  public static final String WEB_CLIENT_ID = "298582902679-k9fbg2tpjehtaa235bkq3s4nolva2t6e.apps.googleusercontent.com";
+  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
